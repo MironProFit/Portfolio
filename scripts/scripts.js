3c876f55f6aa0f5b5c0e90c35ba6646570feec6c
MironProFit <mpf_3d@vk.com>
2025-01-22 11:41:18 +0500

    hover сенсор

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,17 +9,23 @@ function toggleMenu() {
     burger.classList.toggle('active');
 }
 
-// Модальное окно на каждую карточку
+// Модальное окно
+
 const overlay = document.querySelector('.overlay');
 
 document.querySelectorAll('.about-card__btn').forEach(button => {
+
     button.addEventListener('click', () => {
+
         const modalId = button.getAttribute('data-modal');
         const modal = document.getElementById(modalId); 
+
         if (modal) {
         modal.classList.add('active');
-        overlay.classList.add('show'); // Показать затемнение
-        }
+        overlay.classList.add('show');
+            setTimeout(() => {
+                modal.style.visibliti = 'visible'}, 30);
+            }
         console.log(modalId);
         console.log(modal);
     });
@@ -27,29 +33,81 @@ document.querySelectorAll('.about-card__btn').forEach(button => {
 
 document.querySelectorAll('.about-modal__btn.close').forEach(button => {
     button.addEventListener('click', () => {
-        const modal = button.closest('.about-modal'); // Ищем модальное окно по классу
-        overlay.classList.remove('show'); // Скрыть затемнение
+        const modal = button.closest('.about-modal'); 
+
+        overlay.classList.remove('show'); 
         modal.classList.remove('active');
     });
 });
 
 // Слайдер
 
-const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 
-document.getElementById('next').addEventListener('click', () => {
-    slides[currentIndex].classList.remove('img-active');
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add('img-active');
-console.log(currentIndex);
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    currentIndex = (currentIndex + direction + slides.length) % slides.length; 
+    updateSlidePosition();
+    console.log(currentIndex);
+    slide.classList.add('active');
 
-});
 
-document.getElementById('prev').addEventListener('click', () => {
-    slides[currentIndex].classList.remove('img-active');
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    slides[currentIndex].classList.add('img-active');
-console.log(currentIndex);
 
-});
+    
+}
+
+function updateSlidePosition() {
+    const description = document.querySelectorAll('.slide-description__item');
+    const slidesContainer = document.querySelector('.slides');
+    const slides = document.querySelectorAll('.slide');
+
+   
+
+    slides.forEach((slide, index) => {
+
+        if (index === currentIndex) {
+            slide.style.transition = '0.5s';
+            slide.style.opacity = '1';
+            slide.style.cursor = 'pointer';
+            slide.style.border = '3px solid rgba(124, 252, 0, 0.6)';
+            description[index].style.display = 'block';
+        } else {
+            slide.style.transition = '0.5s';
+            slide.style.opacity = '0.2';
+            slide.style.border = '3px solid transparent';
+            description[index].style.display = 'none';
+            slide.style.cursor = 'default';
+
+        }
+        
+        slide.style.boxShadow = `-20px 7px 29px 20px rgba(0, 0, 0, 0.4)`;
+    });
+    
+}
+
+function openFullScreen(image) {
+    const fullScreenImageContainer = document.createElement('div');
+    fullScreenImageContainer.style.position = 'fixed';
+    fullScreenImageContainer.style.top = '50%';
+    fullScreenImageContainer.style.left = '50%';
+    fullScreenImageContainer.style.transform = 'translate(-50%, -50%)';
+    fullScreenImageContainer.style.zIndex = '9999';
+    fullScreenImageContainer.style.height = '80vh'; // Высота контейнера с отступами сверху и снизу
+    fullScreenImageContainer.style.overflow = 'hidden';
+
+    const fullScreenImage = document.createElement('img');
+    fullScreenImage.src = image.src;
+    fullScreenImage.style.height = '100%'; // Заполнение контейнера по высоте
+    fullScreenImage.style.width = 'auto'; // Автоматическая ширина для центровки
+    fullScreenImage.style.objectFit = 'contain';
+
+    fullScreenImageContainer.appendChild(fullScreenImage);
+    overlay.classList.add('show');
+
+    fullScreenImageContainer.onclick = function () {
+        document.body.removeChild(fullScreenImageContainer);
+        overlay.classList.remove('show');
+    };
+
+    document.body.appendChild(fullScreenImageContainer);
+}
